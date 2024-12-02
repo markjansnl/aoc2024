bb75652d890e869036bc58483a118b4d0bedb346
Mark Jans <mark@markjans.nl>
2024-12-02 22:04:50 +0100

    Day 2 part 2

--- src/days/day02/mod.rs
+++ src/days/day02/mod.rs
@@ -1,4 +1,4 @@
-use std::cmp::Ordering;
+use std::cmp::Ordering::*;
 
 use nom::{
     character::complete::{newline, space1, u32},
@@ -7,50 +7,76 @@ use nom::{
 };
 
 use crate::*;
+use Ordering::*;
 
 day! {
     Output = u32,
     Parsed = Vec<Vec<Output>>,
 }
 
+#[derive(PartialEq, Eq)]
+enum Ordering {
+    Increasing,
+    Decreasing,
+}
+
 impl Day {
     #[inline]
     fn part1(reports: Parsed) -> Result<Output> {
-        Ok(reports.into_iter().filter(Self::is_safe).count() as Output)
+        Ok(reports
+            .into_iter()
+            .filter(|report| Self::is_safe(report, false, None))
+            .count() as Output)
     }
 
     #[inline]
-    fn part2(_reports: Parsed) -> Result<Output> {
-        Ok(0)
+    fn part2(reports: Parsed) -> Result<Output> {
+        Ok(reports
+            .into_iter()
+            .filter(|report| Self::is_safe(report, true, None))
+            .count() as Output)
     }
 
-    fn is_safe(report: &Vec<Output>) -> bool {
-        let mut windows = report.windows(2);
-
-        let first = Self::check_ordering(
-            windows
-                .next()
-                .expect("There must be one window in the report"),
-        );
-        if first.is_none() {
-            return false;
+    #[inline]
+    fn is_safe(report: &[Output], tolerance: bool, exclude: Option<usize>) -> bool {
+        if exclude == Some(0) {
+            return Self::is_safe(&report[1..], false, None);
         }
 
-        while let Some(window) = windows.next() {
-            let ordering = Self::check_ordering(window);
-            if ordering.is_none() || ordering != first {
-                return false;
+        let mut iter = report.iter().enumerate();
+        let mut prev = iter.next().unwrap().1;
+        let mut prev_ordering = None;
+
+        for (idx, next) in iter {
+            if Some(idx) == exclude {
+                continue;
+            }
+
+            let next_ordering = Self::check_ordering(*prev, *next);
+            if next_ordering.is_none()
+                || (prev_ordering.is_some() && next_ordering != prev_ordering)
+            {
+                if tolerance {
+                    return Self::is_safe(report, false, Some(idx))
+                        || Self::is_safe(report, false, Some(idx - 1))
+                        || (idx >= 2 && Self::is_safe(report, false, Some(idx - 2)));
+                } else {
+                    return false;
+                }
+            } else {
+                prev = next;
+                prev_ordering = next_ordering;
             }
         }
-
         true
     }
 
-    fn check_ordering(window: &[Output]) -> Option<Ordering> {
-        match window[0].cmp(&window[1]) {
-            Ordering::Equal => None,
-            Ordering::Less => (window[1] - window[0] <= 3).then_some(Ordering::Less),
-            Ordering::Greater => (window[0] - window[1] <= 3).then_some(Ordering::Greater),
+    #[inline]
+    fn check_ordering(prev: Output, next: Output) -> Option<Ordering> {
+        match prev.cmp(&next) {
+            Equal => None,
+            Less => (next - prev <= 3).then_some(Increasing),
+            Greater => (prev - next <= 3).then_some(Decreasing),
         }
     }
 }
