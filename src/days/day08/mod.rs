7a1de17e20b354549eec9719680d2686d3d0e19f
Mark Jans <mark@markjans.nl>
2024-12-08 19:55:04 +0100

    Day 8

--- src/days/day08/mod.rs
+++ src/days/day08/mod.rs
@@ -1,33 +1,128 @@
-use nom::{character::complete::u32, combinator::all_consuming};
+use std::collections::{HashMap, HashSet};
 
 use crate::*;
 
 day! {
-    Output = u32,
-    Parsed = u32,
+    Output = usize,
+    Parsed = Map,
+}
+
+struct Map {
+    width: isize,
+    height: isize,
+    antennas: HashMap<Antenna, Vec<Location>>,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+struct Antenna(char);
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+struct Location {
+    x: isize,
+    y: isize,
+}
+
+impl Map {
+    #[inline]
+    fn contains(&self, location: Location) -> bool {
+        location.x >= 0 && location.x < self.width && location.y >= 0 && location.y < self.height
+    }
 }
 
 impl Day {
     #[inline]
-    fn part1(parsed: Parsed) -> Result<Output> {
-        Ok(parsed)
+    fn part1(map: Parsed) -> Result<Output> {
+        let mut antinodes = HashSet::new();
+        for locations in map.antennas.values() {
+            for (idx, a) in locations.iter().enumerate() {
+                for b in &locations[idx + 1..] {
+                    let antinode1 = Self::next_antinode(*a, *b);
+                    if map.contains(antinode1) {
+                        antinodes.insert(antinode1);
+                    }
+
+                    let antinode2 = Self::next_antinode(*b, *a);
+                    if map.contains(antinode2) {
+                        antinodes.insert(antinode2);
+                    }
+                }
+            }
+        }
+
+        Ok(antinodes.len())
     }
 
     #[inline]
-    fn part2(_parsed: Parsed) -> Result<Output> {
-        Ok(0)
+    fn part2(map: Parsed) -> Result<Output> {
+        let mut antinodes = HashSet::new();
+        for locations in map.antennas.values() {
+            for (idx, a) in locations.iter().enumerate() {
+                for b in &locations[idx + 1..] {
+                    antinodes.insert(*a);
+                    antinodes.insert(*b);
+
+                    let mut k = *a;
+                    let mut l = *b;
+                    let mut m = Self::next_antinode(k, l);
+                    while map.contains(m) {
+                        antinodes.insert(m);
+                        k = l;
+                        l = m;
+                        m = Self::next_antinode(k, l);
+                    }
+
+                    k = *b;
+                    l = *a;
+                    let mut m = Self::next_antinode(k, l);
+                    while map.contains(m) {
+                        antinodes.insert(m);
+                        k = l;
+                        l = m;
+                        m = Self::next_antinode(k, l);
+                    }
+                }
+            }
+        }
+
+        Ok(antinodes.len())
+    }
+
+    #[inline]
+    fn next_antinode(a: Location, b: Location) -> Location {
+        Location {
+            x: 2 * b.x - a.x,
+            y: 2 * b.y - a.y,
+        }
     }
 }
 
 impl Parser {
     #[inline]
     fn parse(input: &'static str) -> Result<Parsed> {
-        Ok(all_consuming(Self::integer)(input)?.1)
-    }
-
-    #[inline]
-    fn integer(s: &'static str) -> IResult<Parsed> {
-        u32(s)
+        let mut antennas: HashMap<Antenna, Vec<Location>> = HashMap::new();
+        let mut width = 0;
+        for (y, line) in input.lines().enumerate() {
+            if y == 0 {
+                width = line.len() as isize;
+            }
+            for (x, c) in line.chars().enumerate() {
+                if c != '.' {
+                    let location = Location {
+                        x: x as isize,
+                        y: y as isize,
+                    };
+                    antennas
+                        .entry(Antenna(c))
+                        .and_modify(|locations| locations.push(location))
+                        .or_insert(vec![location]);
+                }
+            }
+        }
+        Ok(Map {
+            width,
+            height: input.lines().count() as isize,
+            antennas,
+        })
     }
 }
 
@@ -39,7 +134,9 @@ mod tests {
 
     run!(Part2);
 
-    test_example!("example1", Part1, 0);
+    test_example!("example1", Part1, 14);
 
-    test_example!("example1", Part2, 0);
+    test_example!("example1", Part2, 34);
+
+    test_example!("example2", Part2, 9);
 }
