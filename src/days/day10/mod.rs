78bb95786e08ad645b685c76c1e7eb94c4cea430
Mark Jans <mark@markjans.nl>
2024-12-10 11:04:23 +0100

    Day 10

--- src/days/day10/mod.rs
+++ src/days/day10/mod.rs
@@ -1,33 +1,126 @@
-use nom::{character::complete::u32, combinator::all_consuming};
+use std::collections::BTreeSet;
 
 use crate::*;
 
 day! {
-    Output = u32,
-    Parsed = u32,
+    Output = usize,
+    Parsed = Vec<Vec<u8>>,
 }
 
 impl Day {
     #[inline]
-    fn part1(parsed: Parsed) -> Result<Output> {
-        Ok(parsed)
+    fn part1(lines: Parsed) -> Result<Output> {
+        let mut sum = 0;
+        for (y, line) in lines.iter().enumerate() {
+            for (x, &height) in line.iter().enumerate() {
+                if height == 0 {
+                    let mut trail_ends = BTreeSet::new();
+                    Self::trail_score(&lines, x as isize, y as isize, 0, &mut trail_ends);
+                    sum += trail_ends.len();
+                }
+            }
+        }
+        Ok(sum)
     }
 
     #[inline]
-    fn part2(_parsed: Parsed) -> Result<Output> {
-        Ok(0)
+    fn part2(lines: Parsed) -> Result<Output> {
+        let mut sum = 0;
+        for (y, line) in lines.iter().enumerate() {
+            for (x, &height) in line.iter().enumerate() {
+                if height == 0 {
+                    sum += Self::trail_rating(&lines, x as isize, y as isize, 0);
+                }
+            }
+        }
+        Ok(sum)
+    }
+
+    #[inline]
+    fn trail_score(
+        lines: &Parsed,
+        x: isize,
+        y: isize,
+        height: u8,
+        trail_ends: &mut BTreeSet<(isize, isize)>,
+    ) {
+        if height == 9 {
+            trail_ends.insert((x, y));
+        } else {
+            if let Some(h) = Self::get(lines, x, y - 1) {
+                if h == height + 1 {
+                    Self::trail_score(lines, x, y - 1, height + 1, trail_ends);
+                }
+            }
+            if let Some(h) = Self::get(lines, x, y + 1) {
+                if h == height + 1 {
+                    Self::trail_score(lines, x, y + 1, height + 1, trail_ends);
+                }
+            }
+            if let Some(h) = Self::get(lines, x - 1, y) {
+                if h == height + 1 {
+                    Self::trail_score(lines, x - 1, y, height + 1, trail_ends);
+                }
+            }
+            if let Some(h) = Self::get(lines, x + 1, y) {
+                if h == height + 1 {
+                    Self::trail_score(lines, x + 1, y, height + 1, trail_ends);
+                }
+            }
+        }
+    }
+
+    #[inline]
+    fn trail_rating(lines: &Parsed, x: isize, y: isize, height: u8) -> usize {
+        if height == 9 {
+            1
+        } else {
+            let mut score = 0;
+            if let Some(h) = Self::get(lines, x, y - 1) {
+                if h == height + 1 {
+                    score += Self::trail_rating(lines, x, y - 1, height + 1);
+                }
+            }
+            if let Some(h) = Self::get(lines, x, y + 1) {
+                if h == height + 1 {
+                    score += Self::trail_rating(lines, x, y + 1, height + 1);
+                }
+            }
+            if let Some(h) = Self::get(lines, x - 1, y) {
+                if h == height + 1 {
+                    score += Self::trail_rating(lines, x - 1, y, height + 1);
+                }
+            }
+            if let Some(h) = Self::get(lines, x + 1, y) {
+                if h == height + 1 {
+                    score += Self::trail_rating(lines, x + 1, y, height + 1);
+                }
+            }
+            score
+        }
+    }
+
+    #[inline]
+    fn get(lines: &Parsed, x: isize, y: isize) -> Option<u8> {
+        if y < 0 || y >= lines.len() as isize || x < 0 || x >= lines[0].len() as isize {
+            None
+        } else {
+            Some(lines[y as usize][x as usize])
+        }
     }
 }
 
 impl Parser {
     #[inline]
     fn parse(input: &'static str) -> Result<Parsed> {
-        Ok(all_consuming(Self::integer)(input)?.1)
-    }
-
-    #[inline]
-    fn integer(s: &'static str) -> IResult<Parsed> {
-        u32(s)
+        Ok(input
+            .lines()
+            .map(|line| {
+                line.bytes()
+                    .map(|b| if b == 46 { 100 } else { b - 48 })
+                    .collect()
+            })
+            .collect())
     }
 }
 
@@ -39,7 +132,7 @@ mod tests {
 
     run!(Part2);
 
-    test_example!("example1", Part1, 0);
+    test_example!("example1", Part1, 36);
 
-    test_example!("example1", Part2, 0);
+    test_example!("example1", Part2, 81);
 }
