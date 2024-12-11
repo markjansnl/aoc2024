7d3cdaf0b81887b54401f676808066a91507ecbb
Mark Jans <mark@markjans.nl>
2024-12-11 17:22:17 +0100

    Day 11

--- src/days/day11/mod.rs
+++ src/days/day11/mod.rs
@@ -1,33 +1,66 @@
-use nom::{character::complete::u32, combinator::all_consuming};
+use memoize::memoize;
+
+use nom::{
+    character::complete::{space1, u64},
+    combinator::all_consuming,
+    multi::separated_list1,
+};
+use rayon::prelude::*;
 
 use crate::*;
 
 day! {
-    Output = u32,
-    Parsed = u32,
+    Output = usize,
+    Parsed = Vec<u64>,
 }
 
 impl Day {
     #[inline]
-    fn part1(parsed: Parsed) -> Result<Output> {
-        Ok(parsed)
+    fn part1(stones: Parsed) -> Result<Output> {
+        Ok(stones
+            .par_iter()
+            .copied()
+            .map(|stone| count_after_blinks(stone, 25))
+            .sum())
     }
 
     #[inline]
-    fn part2(_parsed: Parsed) -> Result<Output> {
-        Ok(0)
+    fn part2(stones: Parsed) -> Result<Output> {
+        Ok(stones
+            .par_iter()
+            .copied()
+            .map(|stone| count_after_blinks(stone, 75))
+            .sum())
+    }
+}
+
+#[memoize]
+fn count_after_blinks(stone: u64, blinks: u8) -> usize {
+    if blinks == 0 {
+        1
+    } else if stone == 0 {
+        count_after_blinks(1, blinks - 1)
+    } else {
+        let digits = format!("{stone}").len() as u32;
+        if digits > 0 && digits % 2 == 0 {
+            let half_div = 10u64.pow(digits / 2);
+            count_after_blinks(stone / half_div, blinks - 1)
+                + count_after_blinks(stone % half_div, blinks - 1)
+        } else {
+            count_after_blinks(stone * 2024, blinks - 1)
+        }
     }
 }
 
 impl Parser {
     #[inline]
     fn parse(input: &'static str) -> Result<Parsed> {
-        Ok(all_consuming(Self::integer)(input)?.1)
+        Ok(all_consuming(Self::stones)(input)?.1)
     }
 
     #[inline]
-    fn integer(s: &'static str) -> IResult<Parsed> {
-        u32(s)
+    fn stones(s: &'static str) -> IResult<Parsed> {
+        separated_list1(space1, u64)(s)
     }
 }
 
@@ -39,7 +72,5 @@ mod tests {
 
     run!(Part2);
 
-    test_example!("example1", Part1, 0);
-
-    test_example!("example1", Part2, 0);
+    test_example!("example1", Part1, 55312);
 }
