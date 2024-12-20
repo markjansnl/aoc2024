9652075b7a631dfc9b3fd72332e145155e8c65d3
Mark Jans <mark@markjans.nl>
2024-12-20 09:50:17 +0100

    Day 18

--- src/days/day18/mod.rs
+++ src/days/day18/mod.rs
@@ -1,29 +1,161 @@
-use nom::{character::complete::u32, combinator::all_consuming};
+mod util;
+use pathfinding::prelude::astar;
+use util::*;
+
+use nom::{
+    bytes::complete::tag,
+    character::complete::{newline, u8},
+    combinator::{all_consuming, map},
+    multi::separated_list1,
+    sequence::separated_pair,
+};
 
 use crate::*;
 
 day! {
-    Output = u32,
-    Parsed = u32,
+    Output = String,
+    Parsed = Vec<Position>,
+}
+
+struct Config {
+    size: usize,
+    coordinates: usize,
+}
+
+#[derive(Default, PartialEq, Eq, Hash, Clone, Copy)]
+enum Tile {
+    #[default]
+    Empty,
+    Corrupted,
+}
+
+struct MemorySpace {
+    bytes: Grid<71, 71, Tile>,
+}
+
+impl MemorySpace {
+    fn with_size(size: usize) -> Self {
+        Self {
+            bytes: Grid::with_size(size, size),
+        }
+    }
 }
 
 impl Day {
-    fn part1(parsed: Parsed) -> Result<Output> {
-        Ok(parsed)
+    fn part1(coordinates: Parsed) -> Result<Output> {
+        let config = Self::get_config(&coordinates);
+        let mut memory_space = MemorySpace::with_size(config.size);
+        for position in coordinates.iter().copied().take(config.coordinates) {
+            memory_space.bytes[position] = Tile::Corrupted;
+        }
+
+        let start = memory_space.bytes.position(Position::default());
+        let end = memory_space.bytes.position(Position {
+            x: config.size - 1,
+            y: config.size - 1,
+        });
+        let (_, length) = astar(
+            &start,
+            |pos| {
+                [pos.left(), pos.right(), pos.up(), pos.down()]
+                    .into_iter()
+                    .filter_map(|next| {
+                        if let Some(next_pos) = next {
+                            if memory_space.bytes[next_pos] != Tile::Corrupted {
+                                return Some((next_pos, 1));
+                            }
+                        }
+                        None
+                    })
+            },
+            |pos| pos.manhatten_distance(end),
+            |pos| *pos == end,
+        )
+        .unwrap();
+
+        Ok(length.to_string())
     }
 
-    fn part2(_parsed: Parsed) -> Result<Output> {
-        Ok(0)
+    fn part2(coordinates: Parsed) -> Result<Output> {
+        let config = Self::get_config(&coordinates);
+        let mut memory_space = MemorySpace::with_size(config.size);
+        let mut coordinates_iter = coordinates.iter().copied();
+        for _ in 0..config.coordinates {
+            let position = coordinates_iter.next().unwrap();
+            memory_space.bytes[position] = Tile::Corrupted;
+        }
+
+        let end = Position {
+            x: config.size - 1,
+            y: config.size - 1,
+        };
+
+        let mut last_path = Vec::new();
+
+        while let Some(position) = coordinates_iter.next() {
+            memory_space.bytes[position] = Tile::Corrupted;
+
+            if !last_path.is_empty() && last_path.iter().copied().all(|pos| pos != position) {
+                continue;
+            }
+
+            let start_pos = memory_space.bytes.position(Position::default());
+            let end_pos = memory_space.bytes.position(end);
+
+            if let Some((path, _)) = astar(
+                &start_pos,
+                |pos| {
+                    [pos.left(), pos.right(), pos.up(), pos.down()]
+                        .into_iter()
+                        .filter_map(|next| {
+                            if let Some(next_pos) = next {
+                                if memory_space.bytes[next_pos] != Tile::Corrupted {
+                                    return Some((next_pos, 1));
+                                }
+                            }
+                            None
+                        })
+                },
+                |pos| pos.manhatten_distance(end_pos),
+                |pos| *pos == end_pos,
+            ) {
+                last_path = path.into_iter().map(|pos| pos.position()).collect();
+            } else {
+                return Ok(format!("{},{}", position.x, position.y));
+            }
+        }
+        unreachable!()
+    }
+
+    fn get_config(coordinates: &Parsed) -> Config {
+        if coordinates.len() == 25 {
+            Config {
+                size: 7,
+                coordinates: 12,
+            }
+        } else {
+            Config {
+                size: 71,
+                coordinates: 1024,
+            }
+        }
     }
 }
 
 impl Parser {
     fn parse(input: &'static str) -> Result<Parsed> {
-        Ok(all_consuming(Self::integer)(input)?.1)
+        Ok(all_consuming(Self::coordinates)(input)?.1)
     }
 
-    fn integer(s: &'static str) -> IResult<Parsed> {
-        u32(s)
+    fn coordinates(s: &'static str) -> IResult<Parsed> {
+        separated_list1(newline, Self::coordinate)(s)
+    }
+
+    fn coordinate(s: &'static str) -> IResult<Position> {
+        map(separated_pair(u8, tag(","), u8), |(x, y)| Position {
+            x: x as usize,
+            y: y as usize,
+        })(s)
     }
 }
 
@@ -35,7 +167,7 @@ mod tests {
 
     run!(Part2);
 
-    test_example!("example1", Part1, 0);
+    test_example!("example1", Part1, 22);
 
-    test_example!("example1", Part2, 0);
+    test_example!("example1", Part2, "6,1");
 }
---
+++ src/days/day18/util.rs
@@ -0,0 +1,350 @@
+use std::ops::{Index, IndexMut};
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+pub enum Axis {
+    Horizontal,
+    Vertical,
+}
+
+#[derive(Debug, Default, PartialEq, Eq, Hash, Clone, Copy)]
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+pub struct GridPosition<'a, const WIDTH: usize, const HEIGHT: usize, T>
+where
+    T: Default + Copy,
+{
+    grid: &'a Grid<WIDTH, HEIGHT, T>,
+    position: Position,
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+pub struct Grid<const WIDTH: usize, const HEIGHT: usize, T>
+where
+    T: Default + Copy,
+{
+    tiles: [[T; WIDTH]; HEIGHT],
+    width: usize,
+    height: usize,
+}
+
+pub struct GridSlice<'a, const WIDTH: usize, const HEIGHT: usize, T>
+where
+    T: Default + Copy,
+{
+    grid: &'a Grid<WIDTH, HEIGHT, T>,
+    axis: Axis,
+    index: usize,
+}
+
+impl From<(usize, usize)> for Position {
+    fn from((x, y): (usize, usize)) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<'a, const WIDTH: usize, const HEIGHT: usize, T> Default for Grid<WIDTH, HEIGHT, T>
+where
+    T: Default + Copy,
+{
+    fn default() -> Self {
+        Self {
+            tiles: [[Default::default(); WIDTH]; HEIGHT],
+            width: WIDTH,
+            height: HEIGHT,
+        }
+    }
+}
+
+impl<const WIDTH: usize, const HEIGHT: usize, T> Grid<WIDTH, HEIGHT, T>
+where
+    T: Default + Copy,
+{
+    pub fn with_size(width: usize, height: usize) -> Self {
+        Self {
+            tiles: [[Default::default(); WIDTH]; HEIGHT],
+            width,
+            height,
+        }
+    }
+
+    pub fn rows<'a>(&'a self) -> impl Iterator<Item = GridSlice<'a, WIDTH, HEIGHT, T>> {
+        (0..self.height).map(move |y| GridSlice::<'a, WIDTH, HEIGHT, T> {
+            grid: self,
+            axis: Axis::Horizontal,
+            index: y,
+        })
+    }
+
+    pub fn columns<'a>(&'a self) -> impl Iterator<Item = GridSlice<'a, WIDTH, HEIGHT, T>> {
+        (0..self.width).map(move |x| GridSlice::<'a, WIDTH, HEIGHT, T> {
+            grid: self,
+            axis: Axis::Vertical,
+            index: x,
+        })
+    }
+
+    pub fn tiles(&self) -> impl Iterator<Item = T> + use<'_, WIDTH, HEIGHT, T> {
+        self.positions().map(|position| self[position])
+    }
+
+    pub fn positions(&self) -> impl Iterator<Item = Position> + use<'_, WIDTH, HEIGHT, T> {
+        (0..self.width).flat_map(move |y| (0..self.height).map(move |x| (Position { x, y })))
+    }
+
+    pub fn tiles_with_positions(
+        &self,
+    ) -> impl Iterator<Item = (Position, T)> + use<'_, WIDTH, HEIGHT, T> {
+        self.positions().map(|position| (position, self[position]))
+    }
+
+    pub fn len(&self) -> usize {
+        self.width * self.height
+    }
+
+    pub fn position<'a>(&'a self, position: Position) -> GridPosition<'a, WIDTH, HEIGHT, T> {
+        GridPosition {
+            grid: self,
+            position,
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.tiles = [[Default::default(); WIDTH]; HEIGHT];
+    }
+}
+
+impl<'a, const WIDTH: usize, const HEIGHT: usize, T> FromIterator<(Position, T)> for Grid<WIDTH, HEIGHT, T>
+where
+    T: Default + Copy,
+{
+    fn from_iter<I: IntoIterator<Item = (Position, T)>>(iter: I) -> Self {
+        let mut grid = Self::default();
+        for (position, item) in iter {
+            grid[position] = item;
+        }
+        grid
+    }
+}
+
+impl<'a, const WIDTH: usize, const HEIGHT: usize, T> Index<Position> for Grid<WIDTH, HEIGHT, T>
+where
+    T: Default + Copy,
+{
+    type Output = T;
+
+    fn index(&self, position: Position) -> &Self::Output {
+        &self.tiles[position.y][position.x]
+    }
+}
+
+impl<'a, const WIDTH: usize, const HEIGHT: usize, T> Index<GridPosition<'a, WIDTH, HEIGHT, T>> for Grid<WIDTH, HEIGHT, T>
+where
+    T: Default + Copy,
+{
+    type Output = T;
+
+    fn index(&self, grid_position: GridPosition<'a, WIDTH, HEIGHT, T>) -> &Self::Output {
+        &self[grid_position.position]
+    }
+}
+
+impl<'a, const WIDTH: usize, const HEIGHT: usize, T> IndexMut<Position> for Grid<WIDTH, HEIGHT, T>
+where
+    T: Default + Copy,
+{
+    fn index_mut(&mut self, position: Position) -> &mut Self::Output {
+        &mut self.tiles[position.y][position.x]
+    }
+}
+
+impl<'a, const WIDTH: usize, const HEIGHT: usize, T> IndexMut<GridPosition<'a, WIDTH, HEIGHT, T>> for Grid<WIDTH, HEIGHT, T>
+where
+    T: Default + Copy,
+{
+    fn index_mut(&mut self, grid_position: GridPosition<'a, WIDTH, HEIGHT, T>) -> &mut Self::Output {
+        &mut self[grid_position.position]
+    }
+}
+
+impl<'a, const WIDTH: usize, const HEIGHT: usize, T> Index<usize>
+    for GridSlice<'a, WIDTH, HEIGHT, T>
+where
+    T: Default + Copy,
+{
+    type Output = T;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        match self.axis {
+            Axis::Horizontal => {
+                if index >= WIDTH {
+                    panic!("Index out of bounds")
+                }
+                &self.grid.tiles[self.index][index]
+            }
+            Axis::Vertical => {
+                if index >= HEIGHT {
+                    panic!("Index out of bounds")
+                }
+                &self.grid.tiles[index][self.index]
+            }
+        }
+    }
+}
+
+impl<'a, const WIDTH: usize, const HEIGHT: usize, T> GridSlice<'a, WIDTH, HEIGHT, T>
+where
+    T: Default + Copy,
+{
+    pub fn tiles(&self) -> impl Iterator<Item = T> + use<'_, WIDTH, HEIGHT, T> {
+        (0..self.len()).map(|index| self[index])
+    }
+
+    pub fn tiles_with_positions(
+        &self,
+    ) -> impl Iterator<Item = (Position, T)> + use<'_, WIDTH, HEIGHT, T> {
+        (0..self.len()).map(|index| {
+            (
+                match self.axis {
+                    Axis::Horizontal => Position {
+                        x: index,
+                        y: self.index,
+                    },
+                    Axis::Vertical => Position {
+                        x: self.index,
+                        y: index,
+                    },
+                },
+                self[index],
+            )
+        })
+    }
+
+    pub fn len(&self) -> usize {
+        match self.axis {
+            Axis::Horizontal => self.grid.width,
+            Axis::Vertical => self.grid.height,
+        }
+    }
+}
+
+impl Position {
+    pub fn manhatten_distance(&self, rhs: Position) -> usize {
+        self.x.abs_diff(rhs.x) + self.y.abs_diff(rhs.y)
+    }
+
+    pub fn left(&self) -> Position {
+        Position {
+            x: self.x - 1,
+            y: self.y,
+        }
+    }
+
+    pub fn right(&self) -> Position {
+        Position {
+            x: self.x + 1,
+            y: self.y,
+        }
+    }
+
+    pub fn up(&self) -> Position {
+        Position {
+            x: self.x,
+            y: self.y - 1,
+        }
+    }
+
+    pub fn down(&self) -> Position {
+        Position {
+            x: self.x,
+            y: self.y + 1,
+        }
+    }
+}
+
+impl<'a, const WIDTH: usize, const HEIGHT: usize, T> GridPosition<'a, WIDTH, HEIGHT, T>
+where
+    T: Default + Copy,
+{
+    pub fn manhatten_distance(&self, rhs: Self) -> usize {
+        self.position.manhatten_distance(rhs.position)
+    }
+
+    pub fn left(&self) -> Option<Self> {
+        if self.position.x > 0 {
+            Some(GridPosition {
+                grid: self.grid,
+                position: self.position.left(),
+            })
+        } else {
+            None
+        }
+    }
+
+    pub fn right(&self) -> Option<Self> {
+        if self.position.x < self.grid.width - 1 {
+            Some(GridPosition {
+                grid: self.grid,
+                position: self.position.right(),
+            })
+        } else {
+            None
+        }
+    }
+
+    pub fn up(&self) -> Option<Self> {
+        if self.position.y > 0 {
+            Some(GridPosition {
+                grid: self.grid,
+                position: self.position.up(),
+            })
+        } else {
+            None
+        }
+    }
+
+    pub fn down(&self) -> Option<Self> {
+        if self.position.y < self.grid.height - 1 {
+            Some(GridPosition {
+                grid: self.grid,
+                position: self.position.down(),
+            })
+        } else {
+            None
+        }
+    }
+
+    pub fn position(&self) -> Position {
+        self.position
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Grid;
+
+    #[test]
+    fn index() {
+        let mut grid: Grid<10, 10, usize> = Grid::default();
+        for y in 0..grid.height {
+            for x in 0..grid.width {
+                grid.tiles[y][x] = y * 10 + x;
+            }
+        }
+
+        for (y, row) in grid.rows().enumerate() {
+            for (x, t) in row.tiles_with_positions().enumerate() {
+                println!("{y}, {x:?}: {t:?}");
+            }
+        }
+        println!();
+
+        for (position, t) in grid.tiles_with_positions() {
+            println!("{position:?}: {t}");
+        }
+        println!();
+    }
+}
