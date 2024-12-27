b1de0d6f40a8822178f564c7ba8db4609fe6f6fe
Mark Jans <mark@markjans.nl>
2024-12-27 09:44:01 +0100

    Day 25

--- src/days/day25/mod.rs
+++ src/days/day25/mod.rs
@@ -1,15 +1,55 @@
-use nom::{character::complete::u32, combinator::all_consuming};
-
 use crate::*;
 
 day! {
-    Output = u32,
-    Parsed = u32,
+    Output = usize,
+    Parsed = Vec<Schematic>,
+}
+
+type PinHeights = [u8; 5];
+
+enum Schematic {
+    Key(PinHeights),
+    Lock(PinHeights),
+}
+
+impl Schematic {
+    fn is_key(&self) -> bool {
+        matches!(self, Schematic::Key(_))
+    }
+
+    fn is_lock(&self) -> bool {
+        matches!(self, Schematic::Lock(_))
+    }
+
+    fn fits(&self, rhs: &Self) -> bool {
+        if let Schematic::Lock(lock) = self {
+            if let Schematic::Key(key) = rhs {
+                for x in 0..5 {
+                    if lock[x] + key[x] > 5 {
+                        return false;
+                    }
+                }
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+        true
+    }
 }
 
 impl Day {
-    fn part1(parsed: Parsed) -> Result<Output> {
-        Ok(parsed)
+    fn part1(schematics: Parsed) -> Result<Output> {
+        let mut count = 0;
+        for key in schematics.iter().filter(|s| s.is_key()) {
+            for lock in schematics.iter().filter(|s| s.is_lock()) {
+                if lock.fits(key) {
+                    count += 1;
+                }
+            }
+        }
+        Ok(count)
     }
 
     fn part2(_parsed: Parsed) -> Result<Output> {
@@ -19,11 +59,25 @@ impl Day {
 
 impl Parser {
     fn parse(input: &'static str) -> Result<Parsed> {
-        Ok(all_consuming(Self::integer)(input)?.1)
-    }
-
-    fn integer(s: &'static str) -> IResult<Parsed> {
-        u32(s)
+        Ok(input
+            .split("\n\n")
+            .map(|schematic| {
+                let mut pin_heights = PinHeights::default();
+                for line in schematic.lines().skip(1).take(5) {
+                    for (x, c) in line.chars().enumerate() {
+                        if c == '#' {
+                            pin_heights[x] += 1;
+                        }
+                    }
+                }
+                let first = schematic.chars().next().unwrap();
+                if first == '#' {
+                    Schematic::Lock(pin_heights)
+                } else {
+                    Schematic::Key(pin_heights)
+                }
+            })
+            .collect())
     }
 }
 
@@ -33,9 +87,5 @@ mod tests {
 
     run!(Part1);
 
-    run!(Part2);
-
-    test_example!("example1", Part1, 0);
-
-    test_example!("example1", Part2, 0);
+    test_example!("example1", Part1, 3);
 }
